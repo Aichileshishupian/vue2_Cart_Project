039879b040d5453df3bcdf79299c2cf73ecb7365
Aichileshishupian <438382312@qq.com>
2026-03-28 17:29:48 +0800

    上线配置

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-// vue.config.js
 module.exports = {
-  // 这里填你的 GitHub 仓库名，前后必须加斜杠
   publicPath: '/vue2_Cart_Project/'
 }
